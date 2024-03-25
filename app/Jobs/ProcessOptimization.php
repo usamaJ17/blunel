e2ed86d9ad856be7a0196c2814d415de98033c60
Usama Jalal <usamajalal17@gmail.com>
2024-03-25 20:27:13 +0500

    updated optimization code

--- app/Jobs/ProcessOptimization.php
+++ app/Jobs/ProcessOptimization.php
@@ -31,18 +31,18 @@ class ProcessOptimization implements ShouldQueue
      */
     public function handle(): void
     {
-        try{
-            $threshold = Carbon::now()->subHours(23)->subMinutes(57);
-            foreach ($this->batch as $imagePath) {
-                if (!(filemtime($imagePath) > $threshold->timestamp)) {
+        $threshold = Carbon::now()->subHours(23)->subMinutes(57);
+        foreach ($this->batch as $imagePath) {
+            if (!(filemtime($imagePath) > $threshold->timestamp)) {
+                try{
                     $image = Image::make($imagePath);
                     $image->resize($image->width() * 0.70, $image->height() * 0.70);
                     $image->save($imagePath, 45,$image->mime());
-                    Log::info('-');
+                }
+                catch(Exception $ex){
+                    Log::info($ex->getMessage());
                 }
             }
-        }catch(Exception $ex){
-            Log::info($ex->getMessage());
         }
     }
 }
