c9545aed1478da281a3f2166a0ec974e43e350f6
Usama Jalal <usamajalal17@gmail.com>
2024-03-25 14:55:22 +0500

    upd

--- app/Http/Controllers/Web/WebController.php
+++ app/Http/Controllers/Web/WebController.php
@@ -100,8 +100,8 @@ class WebController extends Controller
             $image = Image::make($imagePath);
             // Optimized quality adjustment for size reduction
             // $image->encode($image->mime(), 65);
-            $image->resize($image->width() * 0.75, $image->height() * 0.75);
-            $image->save($imagePath, 50,$image->mime());
+            $image->resize($image->width() * 0.65, $image->height() * 0.65);
+            $image->save($imagePath, 40,$image->mime());
         }
     }
 
